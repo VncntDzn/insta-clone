b05b1aed348c21e91230d37c772fb366240f0491
Vincent Dizon <workvpdizon@gmail.com>
2022-12-24 16:23:03 +0800

    added layout support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import "normalize.css";
+import { ReactElement, ReactNode } from "react";
 import "scss/main.scss";
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
