f79614bd6e1aa1cb8acb206225a2ca970098380b
Vincent Dizon <workvpdizon@gmail.com>
2022-12-28 20:58:03 +0800

    added advertisement styles

--- src/features/auth/advertisement/index.tsx
+++ src/features/auth/advertisement/index.tsx
@@ -1,6 +1,22 @@
 import styles from "./advertisement.module.scss";
 const Advertisement = () => {
-  return <div className={styles.root}>Advertisement</div>;
+  return (
+    <div className={styles.root}>
+      <video className={styles.video} autoPlay muted loop id="myVideo">
+        <source src="/advertisement/friends.mp4" type="video/mp4" />
+      </video>
+      <div className={styles.reviews}>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Advertisement;
