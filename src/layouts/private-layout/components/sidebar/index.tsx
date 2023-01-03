f1c6c6486ec8d2e1bac815457ed592e00fb9e89b
Vincent Dizon <workvpdizon@gmail.com>
2023-01-04 01:09:42 +0800

    added style to active

--- src/layouts/private-layout/components/sidebar/index.tsx
+++ src/layouts/private-layout/components/sidebar/index.tsx
@@ -5,13 +5,25 @@ import styles from "./sidebar.module.scss";
 
 const Sidebar = () => {
   const router = useRouter();
+
+  const handleCheckIfActive = (path: string) => {
+    return router.pathname === path;
+  };
   return (
     <aside className={styles.root}>
       <h1 className={styles.title}>Instaclone</h1>
       {routes.map(({ path, name, inactive_icon, active_icon }) => (
         <Link href={path} className={styles.links} key={name}>
-          <span> {router.pathname === path ? active_icon : inactive_icon}</span>{" "}
-          <span>{name}</span>
+          <span>{handleCheckIfActive(path) ? active_icon : inactive_icon}</span>
+          <span
+            className={`${
+              handleCheckIfActive(path)
+                ? `${styles.active}`
+                : ` ${styles.inactive}`
+            } `}
+          >
+            {name}
+          </span>
         </Link>
       ))}
     </aside>
