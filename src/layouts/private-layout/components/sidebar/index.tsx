33b64da0569031bcda7e7d97fe7bc22acf7498a2
Vincent Dizon <workvpdizon@gmail.com>
2023-01-11 14:19:11 +0800

    adjusted styles

--- src/layouts/private-layout/components/sidebar/index.tsx
+++ src/layouts/private-layout/components/sidebar/index.tsx
@@ -5,7 +5,9 @@ import styles from "./sidebar.module.scss";
 
 import { RiInstagramLine } from "@react-icons/all-files/ri/RiInstagramLine";
 import { useMediaQuery } from "hooks";
-
+import Image from "next/image";
+import Pic from "../../assets/pic.jpg";
+import { RiMenuLine } from "@react-icons/all-files/ri/RiMenuLine";
 const Sidebar = () => {
   const router = useRouter();
   const matches = useMediaQuery("(max-width: 820px)");
@@ -21,21 +23,39 @@ const Sidebar = () => {
       ) : (
         <h1 className={styles.title}>Instaclone</h1>
       )}
+      <div className={styles.linksContainer}>
+        {routes.map(({ path, name, inactive_icon, active_icon }) => (
+          <Link href={path} className={styles.links} key={name}>
+            <span>
+              {handleCheckIfActive(path) ? active_icon : inactive_icon}
+            </span>
+            <span
+              className={`${
+                handleCheckIfActive(path)
+                  ? `${styles.active}`
+                  : ` ${styles.inactive}`
+              } `}
+            >
+              {name}
+            </span>
+          </Link>
+        ))}
+        <div className={styles.profile}>
+          <Image
+            className={styles.avatar}
+            src={Pic}
+            width={30}
+            height={30}
+            alt="Profile"
+          />
+          <p>Profile</p>
+        </div>
+      </div>
 
-      {routes.map(({ path, name, inactive_icon, active_icon }) => (
-        <Link href={path} className={styles.links} key={name}>
-          <span>{handleCheckIfActive(path) ? active_icon : inactive_icon}</span>
-          <span
-            className={`${
-              handleCheckIfActive(path)
-                ? `${styles.active}`
-                : ` ${styles.inactive}`
-            } `}
-          >
-            {name}
-          </span>
-        </Link>
-      ))}
+      <div className={styles.settings}>
+        <RiMenuLine size={30} />
+        <p>More</p>
+      </div>
     </aside>
   );
 };
--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,22 +1,11 @@
 import PrivateLayout from "layouts/private-layout";
 import { ReactElement } from "react";
-import axios from "axios";
+import { useAppSelector } from "store/hooks";
 
 const Feed = () => {
-  const sample = async () => {
-    try {
-      const res = await axios.get("/api/user/create", {
-        email: "vpdizon@gmail.com",
-        password: "123456",
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <div>
       <h1>feed</h1>
-      <button onClick={sample}>create user</button>
     </div>
   );
 };
