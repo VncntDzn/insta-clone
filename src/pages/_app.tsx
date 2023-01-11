43db4b7dcee9307bb64bb0a880978cf9d3455915
Vincent Dizon <workvpdizon@gmail.com>
2023-01-11 12:26:30 +0800

    added redux persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,9 @@ import { ReactElement, ReactNode } from "react";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
 import "scss/main.scss";
-import store from "store";
+import persistor, { store } from "store";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,14 +17,15 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-
 // verify the integrity of the token sent by middleware to verifyToken route api
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(
     <Provider store={store}>
-      <ToastContainer />
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <ToastContainer />
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
-// ...
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
 });
-export default store;
+const persistor = persistStore(store);
+export { store };
+export default persistor;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
