f6059d481c9ef82607f769fdf73d150c2f6350ff
Vincent Dizon <workvpdizon@gmail.com>
2023-01-13 14:28:41 +0800

    Added redux to non pages components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { PersistGate } from "redux-persist/integration/react";
 import "scss/main.scss";
-import persistor, { store } from "store";
+import { persistor, store } from "store";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -19,13 +19,27 @@ type AppPropsWithLayout = AppProps & {
 
 // verify the integrity of the token sent by middleware to verifyToken route api
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ToastContainer />
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
-  );
+  const getLayout = Component.getLayout
+    ? (page: ReactElement) => (
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            {Component.getLayout && (
+              <>
+                <ToastContainer />
+                {Component.getLayout(page)}
+              </>
+            )}
+          </PersistGate>
+        </Provider>
+      )
+    : (page: ReactElement) => (
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <ToastContainer />
+            {page}
+          </PersistGate>
+        </Provider>
+      );
+
+  return getLayout(<Component {...pageProps} />);
 }
