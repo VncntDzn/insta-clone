10e588ccd191c1b0ab351234b25eeb45727ac97c
Vincent Dizon <workvpdizon@gmail.com>
2023-01-26 14:55:50 +0800

    fixed bug when scaling images

--- src/features/create/thumbnail/index.tsx
+++ src/features/create/thumbnail/index.tsx
@@ -2,24 +2,25 @@ import { RiArrowLeftLine } from "@react-icons/all-files/ri/RiArrowLeftLine";
 import Image from "next/image";
 
 import { CgArrowsExpandRight } from "@react-icons/all-files/cg/CgArrowsExpandRight";
-import { RiCheckboxMultipleBlankLine } from "@react-icons/all-files/ri/RiCheckboxMultipleBlankLine";
-import { RiZoomInLine } from "@react-icons/all-files/ri/RiZoomInLine";
 import { RiArrowLeftSFill } from "@react-icons/all-files/ri/RiArrowLeftSFill";
 import { RiArrowRightSFill } from "@react-icons/all-files/ri/RiArrowRightSFill";
+import { RiCheckboxMultipleBlankLine } from "@react-icons/all-files/ri/RiCheckboxMultipleBlankLine";
 import { RiImageLine } from "@react-icons/all-files/ri/RiImageLine";
-import { Avatar, Dialog, Menu } from "common";
+import { RiZoomInLine } from "@react-icons/all-files/ri/RiZoomInLine";
+import { Dialog, Menu } from "common";
 import { useToggle } from "hooks";
 import styles from "./thumbnail.module.scss";
 
 import { RiAddLine } from "@react-icons/all-files/ri/RiAddLine";
 
 import { RiCheckboxBlankLine } from "@react-icons/all-files/ri/RiCheckboxBlankLine";
-import { ChangeEvent, MutableRefObject, useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { useAppDispatch, useAppSelector } from "store/hooks";
 import { SET_FILES } from "store/slices/uploadSlice";
+import Caption from "./components/caption";
 enum PicMenu {
   ORIGINAL = "fill",
   ONE_IS_TO_ONE = "cover",
@@ -40,7 +41,6 @@ const Thumbnail = () => {
   const [toggle, setToggle] = useToggle();
   const [imageFit, setImageFit] = useState<PicMenu | undefined>(undefined);
   const [imageRange, setImageRange] = useState(1);
-  const [isCaptionBoxVisible, setCaptionBoxVisible] = useState(false);
 
   const [selectedItem, setSelectedItem] = useState(0);
   const [menuToggle, setMenuToggle] = useState<any>({
@@ -65,9 +65,6 @@ const Thumbnail = () => {
       );
     },
   });
-  const handleImageRange = (e: ChangeEvent<HTMLInputElement> | undefined) => {
-    setImageRange(parseInt(e!.target!.value));
-  };
   const handleToggle = () => {
     setToggle(!false);
   };
@@ -83,9 +80,10 @@ const Thumbnail = () => {
     };
   };
 
-  const handleSelectedItem = (e, index) => {
-    setSelectedItem(index);
+  const handleImageRange = (e: ChangeEvent<HTMLInputElement> | undefined) => {
+    setImageRange(parseInt(e!.target!.value));
   };
+
   const renderDialog = () => {
     return (
       <Dialog isOpen={toggle} onClose={handleToggle}>
@@ -154,6 +152,9 @@ const Thumbnail = () => {
       </Menu>
     );
   };
+  const handleSelectedItem = (e, index) => {
+    setSelectedItem(index);
+  };
 
   const renderMultipleImageOptions = () => {
     const handleScrollHorizontal = (direction: "right" | "left") => {
@@ -165,7 +166,6 @@ const Thumbnail = () => {
         }
       }
     };
-
     return (
       <Menu className={styles.multipleImages}>
         <div ref={horizontalScrollRef} className={styles.multiImageContainer}>
@@ -211,22 +211,16 @@ const Thumbnail = () => {
     );
   };
 
+  const [isCaptionBoxVisible, setCaptionBoxVisible] = useState(false);
   const handleCaptionVisibility = () => {
     setCaptionBoxVisible(!isCaptionBoxVisible);
+    setMenuToggle({
+      isSizesOpen: false,
+      isRangeOpen: false,
+      isMultipleOpen: false,
+    });
   };
-  const renderCaption = () => {
-    return (
-      <div className={styles.caption}>
-        <Avatar />
-        <textarea
-          autoComplete="off"
-          aria-label="Write a caption..."
-          placeholder="Write a caption..."
-          className={styles.textArea}
-        />
-      </div>
-    );
-  };
+
   return (
     <>
       {menuToggle.isMultipleOpen && renderMultipleImageOptions()}
@@ -247,11 +241,12 @@ const Thumbnail = () => {
       <main className={styles.content}>
         <Carousel
           className={styles.carousel}
-          selectedItem={selectedItem}
           showThumbs={false}
           showStatus={false}
+          selectedItem={selectedItem}
+          onChange={(currentItem) => handleSelectedItem(null, currentItem)}
         >
-          {files.map((file) => (
+          {files.map((file, i) => (
             <section key={file.name} className={styles.image}>
               <Image
                 id={file.name}
@@ -260,41 +255,47 @@ const Thumbnail = () => {
                 fill
                 style={{
                   objectFit: imageFit,
-                  transform: `scale(${imageRange})`,
+                  transform: `${
+                    selectedItem === i ? `scale(${imageRange})` : ""
+                  }`,
                 }}
               />
             </section>
           ))}
         </Carousel>
-        <div className={styles.footer}>
-          <div className={styles.options}>
-            <div onClick={handleMenuToggle}>
-              <CgArrowsExpandRight
-                className={styles.icon}
-                size={20}
-                id="isSizesOpen"
-                style={changeStyle(menuToggle.isSizesOpen)}
-              />
+
+        {!isCaptionBoxVisible ? (
+          <div className={styles.footer}>
+            <div className={styles.options}>
+              <div onClick={handleMenuToggle}>
+                <CgArrowsExpandRight
+                  className={styles.icon}
+                  size={20}
+                  id="isSizesOpen"
+                  style={changeStyle(menuToggle.isSizesOpen)}
+                />
+              </div>
+              <div onClick={handleMenuToggle}>
+                <RiZoomInLine
+                  id="isRangeOpen"
+                  className={styles.icon}
+                  size={20}
+                  style={changeStyle(menuToggle.isRangeOpen)}
+                />
+              </div>
             </div>
             <div onClick={handleMenuToggle}>
-              <RiZoomInLine
-                id="isRangeOpen"
+              <RiCheckboxMultipleBlankLine
                 className={styles.icon}
                 size={20}
-                style={changeStyle(menuToggle.isRangeOpen)}
+                id="isMultipleOpen"
+                style={changeStyle(menuToggle.isMultipleOpen)}
               />
             </div>
           </div>
-          <div onClick={handleMenuToggle}>
-            <RiCheckboxMultipleBlankLine
-              className={styles.icon}
-              size={20}
-              id="isMultipleOpen"
-              style={changeStyle(menuToggle.isMultipleOpen)}
-            />
-          </div>
-        </div>
-        {isCaptionBoxVisible && renderCaption()}
+        ) : (
+          <Caption />
+        )}
       </main>
     </>
   );
