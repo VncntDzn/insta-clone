d52e9286993116d22e599ef3d30f056bf839b81f
Vincent Dizon <workvpdizon@gmail.com>
2023-02-06 14:54:01 +0800

    added styles for react tabs

--- src/features/profile/tabs/index.tsx
+++ src/features/profile/tabs/index.tsx
@@ -7,10 +7,15 @@ import {
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import Posts from "../posts";
+
+import styles from "./tabs.module.scss";
 const ProfileTabs = () => {
   return (
-    <Tabs>
-      <TabList>
+    <Tabs
+      className={styles.root}
+      selectedTabClassName={styles.selectedTabClassName}
+    >
+      <TabList className={styles.tabList}>
         <Tab>
           <RiGridLine size={25} />
         </Tab>
