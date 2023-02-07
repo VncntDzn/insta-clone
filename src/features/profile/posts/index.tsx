d2cd6b72a14c616eedabdd7f483ad6a0228e3292
Vincent Dizon <workvpdizon@gmail.com>
2023-02-07 12:53:56 +0800

    added shallow infinite scroll

--- src/features/profile/posts/index.tsx
+++ src/features/profile/posts/index.tsx
@@ -2,6 +2,7 @@ import { firestore } from "db/client";
 import { collection, getDocs, query } from "firebase/firestore";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useAppSelector } from "store/hooks";
 import styles from "./posts.module.scss";
 
@@ -10,34 +11,45 @@ const Posts = () => {
 
   const [posts, setPosts] = useState([]);
 
+  const fetchPosts = async () => {
+    try {
+      const result: any = [];
+      const queryCollection = query(
+        collection(firestore, `posts/${user?.uid}/post`)
+      );
+      const querySnapshot = await getDocs(queryCollection);
+
+      querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+      });
+
+      setPosts(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    (async function fetchPosts() {
-      try {
-        const result: any = [];
-        const queryCollection = query(
-          collection(firestore, `posts/${user?.uid}/post`)
-        );
-        const querySnapshot = await getDocs(queryCollection);
-
-        querySnapshot.forEach((doc) => {
-          result.push(doc.data());
-        });
-
-        setPosts(result);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
+    fetchPosts();
   }, [user?.uid]);
   return (
-    <div className={styles.root}>
-      {/* TODO: Add infinite scroll */}
+    <InfiniteScroll
+      className={styles.root}
+      dataLength={posts.length} //This is important field to render the next data
+      next={fetchPosts}
+      hasMore={false}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
       {posts.map((post, i) => (
         <div className={styles.container} key={i}>
           <Image alt={post} src={post.imageURL[0]} fill />
         </div>
       ))}
-    </div>
+    </InfiniteScroll>
   );
 };
 
