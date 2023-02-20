156566d9ddd6e713796e3bf147aecf373aa58541
Vincent Dizon <workvpdizon@gmail.com>
2023-02-21 01:09:36 +0800

    added components to profile

---
+++ src/features/posts/header/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from "common/avatar";
+import { RiArrowLeftSLine } from "react-icons/ri";
+import { RxDotsHorizontal } from "react-icons/rx";
+import styles from "./posts.module.scss";
+
+const PostsHeader = () => {
+  return (
+    <section className={styles.root}>
+      <div className={styles.header}>
+        <RiArrowLeftSLine size={35} />
+        <strong className={styles.title}>Photo</strong>
+      </div>
+      <div className={styles.user}>
+        <div className={styles.avatar}>
+          <Avatar height={40} width={40} />
+        </div>
+        <div className={styles.name}>
+          <strong>vincent.tsx</strong>
+          <small>Location...</small>
+        </div>
+        <RxDotsHorizontal className={styles.dotIcon} size={20} />
+      </div>
+    </section>
+  );
+};
+
+export default PostsHeader;
---
+++ src/features/posts/index.ts
@@ -0,0 +1,1 @@
+export { default as PostsHeader } from "./header";
