a0ca038ce5b8208a6858568c4ae6fb06c32f8a07
Vincent Dizon <workvpdizon@gmail.com>
2023-03-02 01:56:02 +0800

    created change password

---
+++ src/features/account/change-password/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./change-password.module.scss";
+
+const ChangePassword = () => {
+  return <div className={styles.root}>ChangePassword</div>;
+};
+
+export default ChangePassword;
