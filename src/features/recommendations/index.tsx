89a41b80914ba0b96c1b4002c76254960021e5cb
Vincent Dizon <workvpdizon@gmail.com>
2023-03-07 14:49:03 +0800

    added follow other user

--- src/features/recommendations/index.tsx
+++ src/features/recommendations/index.tsx
@@ -1,22 +1,66 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useCallback, useEffect, useState } from "react";
 import styles from "./recommendations.module.scss";
 import { Avatar } from "common";
+import { toast } from "react-toastify";
+import { firestore } from "db/client";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { useAppSelector } from "store/hooks";
 const Recommendations = () => {
+  const currentUser = useAppSelector((state) => state.user.user);
+  const [users, setUsers] = useState([]);
+  const fetchUsers = useCallback(async () => {
+    try {
+      const result: any = [];
+      const querySnapshot = await getDocs(collection(firestore, `users`));
+
+      querySnapshot.forEach((doc) => {
+        // remove current user from adding to users array
+        if (doc.id !== currentUser?.uid) result.push(doc.data());
+      });
+
+      setUsers(result);
+    } catch (error) {
+      toast.error("Error fetching users");
+    }
+  }, [currentUser?.uid]);
+
+  const handleFollowUser = async ({ uid }) => {
+    try {
+      await addDoc(
+        collection(firestore, `following/${currentUser!.uid}/users`),
+        {
+          uid,
+        }
+      );
+      toast.success("Followed!");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
   return (
     <div className={styles.root}>
       <p className={styles.textHeader}>Suggestions for you</p>
       <div className={styles.container}>
-        {[1, 2, 3, 5, 6, 1, 2, 3132, 1, 23, 123, 123, 123].map((i) => (
-          <div className={styles.users} key={i}>
+        {users.map(({ displayName, uid, fullName }) => (
+          <div className={styles.users} key={uid}>
             <div className={styles.avatarContainer}>
               <Avatar height={50} width={50} />
               <div className={styles.name}>
-                <strong>vincent.tsx</strong>
-                <small>Vincent Dizon</small>
+                <strong>{displayName}</strong>
+                <small>{fullName || ""}</small>
               </div>
             </div>
             <div>
-              <button className={styles.followBtn}>Follow</button>
+              <button
+                className={styles.followBtn}
+                onClick={() => handleFollowUser({ uid })}
+              >
+                Follow
+              </button>
             </div>
           </div>
         ))}
