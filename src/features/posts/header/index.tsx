c82964e847494695b60e2ffa61d308ada0ee0da6
Vincent Dizon <workvpdizon@gmail.com>
2023-04-10 22:12:50 +0800

    adjusted styles to post header

--- src/features/posts/header/index.tsx
+++ src/features/posts/header/index.tsx
@@ -1,22 +1,19 @@
 import Avatar from "common/avatar";
 import { RxDotsHorizontal } from "react-icons/rx";
-import { useAppSelector } from "store/hooks";
 import styles from "./header.module.scss";
 import { PostHeaderProps } from "./post.header.types";
 
 const PostsHeader = ({ name, userPhoto, uid, location }: PostHeaderProps) => {
   return (
     <section className={styles.root}>
-      <div className={styles.user}>
-        <div className={styles.avatar}>
-          <Avatar height={40} width={40} uid={uid} />
-        </div>
-        <div className={styles.name}>
-          <strong>{name}</strong>
-          <small>{location ?? ""}</small>
-        </div>
-        <RxDotsHorizontal className={styles.dotIcon} size={20} />
+      <div className={styles.avatar}>
+        <Avatar height={40} width={40} uid={uid} />
       </div>
+      <div className={styles.name}>
+        <strong>{name}</strong>
+        <p>{location ?? ""}</p>
+      </div>
+      <RxDotsHorizontal size={20} />
     </section>
   );
 };
